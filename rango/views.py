f7b404fb5785893d9cdc9d8cb7be97b1fd8fd63d
Petros Kitazos <2526547k@student.gla.ac.uk>
2022-02-13 17:35:26 +0000

    added Templates directory

---
+++ rango/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+
+def about(request):
+    return HttpResponse("<a href='/'> Index</a>\n"
+                        "Rango says here is the about page.")
+
