2d27528182f11a95a78c3a2db46e4beb59c860b8
Petros Kitazos <2526547k@student.gla.ac.uk>
2022-02-16 17:33:41 +0000

    urls still not working for Pages

--- rango/models.py
+++ rango/models.py
@@ -3,13 +3,14 @@ from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = 'Categories'
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -20,8 +21,11 @@ class Category(models.Model):
 
 
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField
     views = models.IntegerField(default=0)
 
